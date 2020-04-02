dae54edc1adcc67d565829057345a8365ad096d7
Rusbert Sánchez <rusbertsanchez8@gmail.com>
2020-04-01 18:55:03 -0800

    IsMultiple

--- Practica #2/Mes #1/Algoritmo #1 (Es Multiplo)/javascript/js.js
+++ Practica #2/Mes #1/Algoritmo #1 (Es Multiplo)/javascript/js.js
@@ -1,14 +1,28 @@
+/* VALORES: */
+var n = parseInt (6);
 
-isMultiple (10, 3);
+var m = parseInt (3);
+
+/* VERIFICANDO: */
+if (n <= 0 || m <= 0) {
+    console.log ("Introduce valores positivos.");
+} else if (n >= 1 && m >= 1) {
+    isMultiple (n, m);
+} else {
+    console.log ("Valor inválido.");
+}
+
+/* FUNCIONES: */
+
 function isMultiple (n, m) {
     let x = 1;
-    while (n!=m*x   ||n==m*x ) {
+    while (n != m * x || n == m * x) {
         if (n == m * x ) {
-            return console.log ("true");
+            return console.log ("True");
         } else if (n > m * x){
             x++;
         } else {
-            return console.log ("false");
+            return console.log ("False");
         }      
     } 
 } 
